cd781fd09363318733e9ed5c8d01d2d2be6e034d
RomanHalaburda <mtbrider94@gmail.com>
2017-03-27 20:52:10 +0300

    Created project.

---
+++ vsrpp_5/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vsrpp_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            Разработайте MDI – приложение, позволяющее вводить
+            информацию о студентах: фамилия, имя, факультет, группа. Приложение
+            должно обеспечивать возможность создания для каждого студента своей
+            формы. Организовать сохранение информации в файл.
+         */
+    }
+}
